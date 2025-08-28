ed406a511dbd16576bae4d27d945645fa0a4d7de
kalenderceus01 <kalenderx1981@gmail.com>
2025-08-28 01:57:53 +0100

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,14 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { User } from "../types";
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader?.split(" ")[1];
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
     if (err) return res.sendStatus(403);
-    (req as any).user = user;
+    (req as any).user = decoded as User;
     next();
   });
 };
